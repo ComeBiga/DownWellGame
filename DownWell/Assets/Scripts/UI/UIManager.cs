5f5eed659fa3bc6e44e8be6b4b0d8d9b6b676fd3
XZY-83 <tnwl8279@gmail.com>
2021-10-02 19:48:55 +0900

    Modify Start scene

--- DownWell/Assets/Scripts/UI/UIManager.cs
+++ DownWell/Assets/Scripts/UI/UIManager.cs
@@ -5,22 +5,18 @@ using UnityEngine;
 public class UIManager : MonoBehaviour
 {
     GameObject startPanel;
-    GameObject charPanel;
 
 
     void Awake()
     {
         startPanel = GameObject.Find("startPanel");
-        charPanel = GameObject.Find("charPanel");
         
         startPanel.SetActive(true);
-        charPanel.SetActive(false);
     }
 
     public void startBtn()
     {
         startPanel.SetActive(false);
-        charPanel.SetActive(true);
     }
 
     public void exitBtn()
