87b5a343dbd0279646b9b62557abdeb32a8dcb10
XZY-83 <tnwl8279@gmail.com>
2021-10-02 18:47:04 +0900

    Start Scene

---
+++ DownWell/Assets/Scripts/UI/SelectMan.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SelectMan : MonoBehaviour
+{
+    GameObject character;
+
+    GameObject rbtn;
+    GameObject lbtn;
+
+    bool moveR;
+    bool moveL;
+
+    int moveX = 0;
+
+    public static int charNum = 0;
+
+    void Awake()
+    {
+        character = GameObject.Find("character");
+        rbtn = GameObject.Find("rButton");
+        lbtn = GameObject.Find("lButton");
+    }
+
+    void Update()
+    {
+        btnVisible();
+        if (moveR || moveL)
+        {
+            rbtn.GetComponent<Button>().enabled = false;
+            lbtn.GetComponent<Button>().enabled = false;
+        }
+        else
+        {
+            rbtn.GetComponent<Button>().enabled = true;
+            lbtn.GetComponent<Button>().enabled = true;
+        }
+
+        if (moveX >= 100)
+        {
+            moveR = false;
+            moveL = false;
+            moveX = 0;
+        }
+
+        if (moveR)
+        {
+            moveX += 1;
+            character.transform.localPosition += new Vector3(-1, 0, 0);
+        }
+        else if (moveL)
+        {
+            moveX += 1;
+            character.transform.localPosition += new Vector3(1, 0, 0);
+        }
+    }
+
+    public void selectbtn()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void arrowbtn(string arrow)
+    {
+        switch (arrow)
+        {
+            case "right":
+                moveR = true;
+                charNum += 1;
+                break;
+            case "left":
+                moveL = true;
+                charNum -= 1;
+                break;
+        }
+    }
+
+    void btnVisible()
+    {
+        if (charNum == 0)   lbtn.SetActive(false);
+        else if (charNum == 2)  rbtn.SetActive(false);
+        else
+        {
+            rbtn.SetActive(true);
+            lbtn.SetActive(true);
+        }
+    }
+}
