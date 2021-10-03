a371bb9b458bef94cf439c9bcff60aa07d0fa2dd
ComeBiga <36800639+ComeBiga@users.noreply.github.com>
2021-10-03 12:33:58 +0900

    Revert "Modify Start scene"
    This reverts commit 5f5eed659fa3bc6e44e8be6b4b0d8d9b6b676fd3.

--- DownWell/Assets/Scripts/UI/UIManager.cs
+++ DownWell/Assets/Scripts/UI/UIManager.cs
@@ -5,18 +5,22 @@ using UnityEngine;
 public class UIManager : MonoBehaviour
 {
     GameObject startPanel;
+    GameObject charPanel;
 
 
     void Awake()
     {
         startPanel = GameObject.Find("startPanel");
+        charPanel = GameObject.Find("charPanel");
         
         startPanel.SetActive(true);
+        charPanel.SetActive(false);
     }
 
     public void startBtn()
     {
         startPanel.SetActive(false);
+        charPanel.SetActive(true);
     }
 
     public void exitBtn()
