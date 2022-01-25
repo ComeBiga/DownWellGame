00dfe7da5f6e8f69f7578bbdede6eb38eceda7b9
ComeBiga <36800639+ComeBiga@users.noreply.github.com>
2022-01-25 19:20:09 +0900

    Changed Brush Button

--- DownWell/Assets/1.Scripts/ScriptableObjects/LevelObject.cs
+++ DownWell/Assets/1.Scripts/ScriptableObjects/LevelObject.cs
@@ -8,5 +8,31 @@ public class LevelObject : ScriptableObject
     public int code;
     public new string name;
     public Sprite sprite;
+    public bool sliced = false;
     public int score;
+
+    public Texture2D GetTexture2D()
+    {
+        if (sliced)
+            return GetCroppedTexture();
+        else
+            return sprite.texture;
+    }
+
+    public Texture2D GetCroppedTexture()
+    {
+        var ct = new Texture2D((int)sprite.rect.width, (int)sprite.rect.height);
+
+        var pixels = sprite.texture.GetPixels((int)sprite.textureRect.x,
+                                         (int)sprite.textureRect.y,
+                                         (int)sprite.textureRect.width,
+                                         (int)sprite.textureRect.height);
+
+        ct.SetPixels(pixels);
+        ct.Apply();
+
+        return ct;
+    }
 }
+
+
