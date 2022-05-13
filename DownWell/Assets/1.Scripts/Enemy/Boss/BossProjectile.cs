7b0aedc4470c2fb46d058f70d0041b289ea3ca6c
xzy <tnwl8279@gmail.com>
2022-05-14 01:52:36 +0900

    Stage4 Boss Drill

--- DownWell/Assets/1.Scripts/Enemy/Boss/BossProjectile.cs
+++ DownWell/Assets/1.Scripts/Enemy/Boss/BossProjectile.cs
@@ -59,7 +59,7 @@ public class BossProjectile : MonoBehaviour
             if (transform.position.y < Camera.main.transform.position.y - Camera.main.orthographicSize
                 || transform.position.y > Camera.main.transform.position.y + Camera.main.orthographicSize
                 || transform.position.x < Camera.main.transform.position.x - Camera.main.orthographicSize * 9 / 16
-                || transform.position.x < Camera.main.transform.position.x - Camera.main.orthographicSize * 9 / 16)
+                || transform.position.x > Camera.main.transform.position.x + Camera.main.orthographicSize * 9 / 16)
                 break;
 
                 transform.localPosition += new Vector3(direction.x, direction.y) * speed * Time.deltaTime;
